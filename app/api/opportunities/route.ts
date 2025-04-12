ce5511fe1c8e39c32b3c1b58b22e76ef8e17483f
Your Name <your.email@example.com>
2025-04-12 20:02:53 +0200

    fix: move NextAuth options to separate file

--- app/api/opportunities/route.ts
+++ app/api/opportunities/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import { PrismaClient } from '@prisma/client'
 import { getServerSession } from 'next-auth'
-import { options } from '../auth/[...nextauth]/route'
+import { options } from '../auth/[...nextauth]/options'
 
 const prisma = new PrismaClient()
 
